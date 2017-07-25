d86574d9da438c323d2fdbbd3d4c10eb00234994
Cassie Hilbig <cassiehilbig@gmail.com>
2017-07-25 12:59:30 -0400

    use ajax asyncronously to get rid of deprication warning

--- d2llabs-carousel.js
+++ d2llabs-carousel.js
@@ -1,41 +1,40 @@
 $(document).ready(function(){
-  $.ajaxSetup({
-    async: false
-  });
   var backgroundColours = ['green', 'blue', 'purple', 'orange'];
   var moduleId = GetQueryStringParams('moduleId');
   var carousel = $('<div class="carousel-inner" role="listbox">');
   var orgUnitId = GetQueryStringParams('ou');
 
-  var moduleAPI = '/d2l/api/le/1.25/' + orgUnitId + '/content/modules/' + moduleId + '/structure/';//ADD SLASH BACK TO END
-  var apiRes = makeAPIcall('GET', moduleAPI);
-  console.log(apiRes);
   var i = 0;
-  apiRes.forEach(function(topic) {
-    if (topic.Type === 1) {
-      var colour = backgroundColours[i%backgroundColours.length];
-      var topicAPI = '/d2l/api/le/1.25/' + orgUnitId + '/content/topics/' + topic.Id;
-      var topicAPIRes = makeAPIcall('GET', topicAPI);
-      $('<div class="' + "item " + colour + '">' +
-        '<div class="pane center-block">' +
-        '<h2>' + topicAPIRes.Title + '</h2>' +
-        '<p>' + topicAPIRes.Description.Text + '</p>' +
-        '<a class="btn btn-default" href="/d2l/le/content/' + orgUnitId + '/viewContent/' + topicAPIRes.Id + '/View" target="_parent">Learn More</a></div>' +
-        '</div>').appendTo('.carousel-inner');
-      $('<li data-target="#myCarousel" data-slide-to="' + (i+1) + '"></li>').appendTo('.carousel-indicators');
-      i++;
-    }
-  });
-  $('.item').first().addClass('active');
-  $('.carousel-indicators > li').first().addClass('active');
-  $('#carousel-example-generic').carousel();
+  function moduleSuccessCallback (apiRes) {
+    apiRes.forEach(function (topic) {
+      if (topic.Type === 1) {
+        function addTopic(topicAPIRes) {
+          var colour = backgroundColours[i % backgroundColours.length];
+          $('<div class="' + "item " + colour + '">' +
+            '<div class="pane center-block">' +
+            '<h2>' + topicAPIRes.Title + '</h2>' +
+            '<p>' + topicAPIRes.Description.Text + '</p>' +
+            '<a class="btn btn-default" href="/d2l/le/content/' + orgUnitId + '/viewContent/' + topicAPIRes.Id + '/View" target="_parent">Learn More</a></div>' +
+            '</div>').appendTo('.carousel-inner');
+          $('<li data-target="#myCarousel" data-slide-to="' + (i + 1) + '"></li>').appendTo('.carousel-indicators');
+          i++;
+          $('.item').first().addClass('active');
+          $('.carousel-indicators > li').first().addClass('active');
+          $('#carousel-example-generic').carousel();
+        }
+        var topicAPI = '/d2l/api/le/1.25/' + orgUnitId + '/content/topics/' + topic.Id;
+        makeAPIcall('GET', topicAPI, addTopic);
+      }
+    });
+  }
+
+  var moduleAPI = '/d2l/api/le/1.25/' + orgUnitId + '/content/modules/' + moduleId + '/structure/';
+  makeAPIcall('GET', moduleAPI, moduleSuccessCallback);
 });
 
 function GetQueryStringParams(sParam) {
   var obj = window;
   var sPageURL = obj.location.search.substring(1);
-  console.log(obj.location);
-  console.log(sPageURL);
   var sURLVariables = sPageURL.split('&');
   for (var i = 0; i < sURLVariables.length; i++)
   {
@@ -47,19 +46,13 @@ function GetQueryStringParams(sParam) {
   }
 }
 
-function makeAPIcall(method, api) {
-  var res = {};
-  function result_success (data) {
-    res = data;
-  }
-
+function makeAPIcall(method, api, result_success) {
   $.ajax(api, {
     method: method,
     xhrFields: {withCredentials: true},
     success: result_success,
     error: result_error
   });
-  return res;
 }
 
 function result_error(err) {
